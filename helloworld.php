65c2b759a88c7f24c034290463465d6736b34d11
streuli <111392716+streuli@users.noreply.github.com>
2025-01-24 15:57:29 -0700

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--This is from page 16 of the class text-->
+    <meta charset="utf-8">
+    <title> Hello, World!</title>
+</head>
+
+<body>
+    <h1>My first PHP page</h1>
+    <?PHP
+    echo"<p>Hello world...</p>\n";
+    ?>
+</body>
+</html>
