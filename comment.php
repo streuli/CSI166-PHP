8c02a0614d25c86f739a4ff24513d6c2d26c355a
streuli <111392716+streuli@users.noreply.github.com>
2025-01-26 11:02:56 -0700

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <h1>Comments</h1>
+</head>
+
+<body>
+    <?PHP
+    echo "There is a comment here: ";
+    //This is a single line comment.
+
+    /*THis is a multiple line comment..
+      This is a multiple line comment*/
+
+    ?>
+</body>
+</html>
