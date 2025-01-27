89ecb4d297ad4685694c401527256945effdf364
streuli <111392716+streuli@users.noreply.github.com>
2025-01-27 12:58:41 -0700

    Add files via upload

---
+++ varaibles.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Personal Info</title>
+</head>
+<body>
+  <pre>
+    <?PHP
+
+    $name = "Sebastian Streuli";
+    $age = 22;
+    $address = "1534 W Sun Circle Glendale, AZ 85023";
+    $phone = 6235544874;
+
+    print "<p> My is $name and I am $age years old <br> I live at $address <br> My phone number is $phone<p>";
+
+
+    ?>
+  </pre>
+
+</body>
+</html>
