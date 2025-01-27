258aff8b6b86933a5faa7ed024af7e6a8ecc8ed7
streuli <111392716+streuli@users.noreply.github.com>
2025-01-27 12:30:31 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Predefined Variables</title>
+</head>
+<body>
+  <pre>
+    <?PHP
+    print_r($_SERVER );
+
+    ?>
+  </pre>
+
+</body>
+</html>
