7786dbd432fed8ef1f7f3c4d2ee762a8eb18d223
streuli <111392716+streuli@users.noreply.github.com>
2025-02-03 15:38:43 -0700

    Add files via upload

---
+++ random_number.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Random Numbers</title>
+    </head>
+    <body>
+        <?php
+
+        $num = mt_rand(10, 100);
+
+        echo"Your random number is: $num";
+
+        ?>
+    </body>
+</html>
