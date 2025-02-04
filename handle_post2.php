fe9d43c17c24c1cf72a21c2b5fc95ae5c92cc758
streuli <111392716+streuli@users.noreply.github.com>
2025-02-04 16:24:06 -0700

    Add files via upload

---
+++ handle_post2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Forum Posting</title>
+    </head>
+    <body>
+        <?php
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $posting = nl2br($_POST['posting'], false);
+
+        $name = $first_name . ' '. $last_name;
+        
+        print "<div> Thank you, $name, for your posting: <p>$posting</P></div>";
+
+        ?>
+    </body>
+</html>
