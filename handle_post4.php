32c702e5b71c6c1165a30af14084a46e465cc96a
streuli <111392716+streuli@users.noreply.github.com>
2025-02-04 16:29:50 -0700

    Add files via upload

---
+++ handle_post4.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Forum Posting</title>
+    </head>
+    <body>
+        <?php
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $posting = nl2br($_POST['posting'], false);
+
+        $name = $first_name . ' '. $last_name;
+
+        $html_post = htmlentities($_POST['posting']);
+        $strip_post = strip_tags($_POST['posting']);
+        
+        print "<div> Thank you, $name, for your posting:
+            <p>$posting</P></div>";
+
+        $name = urlencode($name);
+        $email = urlencode($_POST [ "email"]);
+        print "<p> Click <a href=\"thanks.php? name=$name&email=$email\">here</a> to continue.</P>";
+
+        ?>
+    </body>
+</html>
