2dfd7a1c04a37eb6d56774e2d5e1285caf564d4f
streuli <111392716+streuli@users.noreply.github.com>
2025-02-11 10:29:22 -0700

    Add files via upload

---
+++ handle_reg.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Registration</title>
+    </head>
+    
+    <body>
+        <h1>Registration Results</h1>
+        <?php
+
+        ini_set('log_errors', 1);
+        ini_set('error_log', 'errors.log');
+
+
+      
+        $okay = true;
+
+        if ($okay) {
+            print '<p>You have been successfully registered.</p>';
+        }
+        ?>
+        
+    </body>
+</html>
