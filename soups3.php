e9a2510ca1c9e8ff02ac2daa8ba6f02d9cb4e034
streuli <111392716+streuli@users.noreply.github.com>
2025-02-14 15:03:20 -0700

    Add files via upload

---
+++ soups3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang  = "en">
+    <head>
+        <meta charset="utf-8">
+        <title>No Soup for You!</title>
+    </head>
+    <body>
+        <h1>Mmm...soups</h1>
+        <?php
+        ini_set('log_errors', 1);
+        ini_set('error_log', 'errors.log');
+
+         $soups = [
+            'Monday'=> 'Clam Chowder',
+            'Tuesday'=> 'White Chicken Chili',
+            'Wednesday'=> 'Vegetarian',
+            'Thursday'=> 'Chicken Noodle',
+            'Friday'=> 'Tomato',
+            'Saturday'=> 'Cream of Brocoli',
+
+         ];
+
+        // Print each key and value
+        foreach ($soups as $day => $soup) {
+            print "<p>$day: $soup</p>\n";
+        }
+
+        ?>
+
+    </body>
+</html>
