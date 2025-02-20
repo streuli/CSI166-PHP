0a601f3c72173db55280384032c74d1e42121608
streuli <111392716+streuli@users.noreply.github.com>
2025-02-20 08:50:44 -0700

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,14 @@
+<?php
+include 'H:\XAMPP\htdocs\templates\header3.html';
+?>
+
+<h2>Welcome to a J.D. Salinger Fan Club</h2>
+<p>
+    Gary Kildall's story is fascinating but also sad in many ways. He contributed so much to early computing with his creations like BIOS and CP/M, yet he doesn't get the recognition he deserves. 
+    It’s unfortunate that the IBM deal didn’t work out and that myths like the “flying” story overshadow his achievements. It’s good that some sources are setting the record straight, showing that 
+    he was more than just the guy who missed out on a big moment. His innovations shaped personal computing, and it's unfortunate that bitterness and personal struggles later affected his life.
+
+</p>
+<?php
+include 'H:\XAMPP\htdocs\templates\footer3.html';
+?>
