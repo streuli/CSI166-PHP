fdce3a9fcab88d0fdc4826b9cefc87fba016797d
streuli <111392716+streuli@users.noreply.github.com>
2025-02-20 09:22:44 -0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+define("TITLE","Welcome to J.D. Salinger Fan Club!");
+include 'templates/header3.html'
+
+
+
+?>
+<h2>Welcome to the J.D. Salinger Fan Club!</h2>
+<p>You've successfully logged in and can now take advantage of everything the site has to offer.</p>
+
+<?php include 'H:\XAMPP\htdocs\templates\footer3.html'?>
