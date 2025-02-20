e2b5737c733b858505656f013f2ac1f9f20cee4d
streuli <111392716+streuli@users.noreply.github.com>
2025-02-20 08:53:22 -0700

    Add files via upload

--- books.php
+++ books.php
@@ -1,34 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title> Larry Ullman's Books and Chapters</title>
-    </head>
-    <body>
-        <h1>Some of Larry Ullman's Books</h1>
-        <?php
-        $phpvqs = [1 => 'Getting Started with PHP', ' Variables', 'HTML Forms and PHP', 'Using Numbers'];
+<?php
+define('TITLE', 'Books by J.D. Salinger');
+include 'H:\XAMPP\htdocs\templates\header2.html';
+?>
 
-        $phpadv = [1 => 'Advance PHP Techniques', 'Developing Web Application', 'Advance Database Concepts', 'Basic Object-Orientated Programming'];
-
-        $phpmysql = [1 => 'Introduction to PHP', 'Programming with PHP', 'Creating Dynamic Web Sites', 'Introduction to MySQL'];
-
-        // Multidimensional array
-
-        $books = [
-            'PHP VQP' => $phpvqs,
-            'PHP Advanced VQP'=> $phpadv,
-            'PHP and MySQL VQP'=> $phpmysql
-        ];
-
-        print "<p>The third chapter of my first book is <i>{$books['PHP VQP'][3]}</i>.</p>";
-        print "<p>The first chapter of my second book is <i>{$books['PHP Advanced VQP'][1]}</i>.</p>";
-        print "<p>The fourth chapter of my fourth book is <i>{$books['PHP and MySQL VQP'][4]}</i>.</p> ";
-
-        foreach ($books as $key => $value) {
-           print "<p>$key: $value</p>\n";
-        }
+<h2>J.D Salinger's Books</h2>
+<ul>
+    <li>THe Catcher in the Rye</li>
+    <li>Nine Stories</li>
+    <li>Rise High the Roof Beam!, Carpenters and Seymour: An Introduction</li>
     
-        ?>
-    </body>
-</html>
+</ul>
+
+<?php include 'H:\XAMPP\htdocs\templates\footer2.html';
+?>
