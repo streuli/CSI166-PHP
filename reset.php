7f3868cb679de98a7bdea6f0df8e6119eb597e36
streuli <111392716+streuli@users.noreply.github.com>
2025-02-21 15:34:11 -0700

    Add files via upload

---
+++ reset.php
@@ -0,0 +1,18 @@
+<?php
+
+setcookie('font_size','', time() -600,'/');
+setcookie('font_color','', time() -600,'/');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Reset Your Settings</title>
+    </head>
+    <body>
+        <p>Your settings have been reset!
+        feel free to <a href="view_settings.php">customize</a> them again.</p>
+
+    </body>
+</html>
