6bf4c2800d9eb50515e03491c5587bfe62c9e5ab
streuli <111392716+streuli@users.noreply.github.com>
2025-03-01 16:36:48 -0700

    Add files via upload

---
+++ add_quotes.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Add a Quotation</title>
+    </head>
+    <body>
+        <?php
+
+        $file = 'H:\XAMPP\htdocs\web root\quotes.txt';
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            if (!empty($_POST['quote']) && ($_POST['quote'] != 'Enter your quotation here.')) {
+
+                if (is_writable($file)) {
+                    file_put_contents($file, $_POST['quote'] . PHP_EOL, FILE_APPEND);
+
+                    print '<p>Your quotation has been stored</p>';
+                } else {
+                    print '<p style="color:red;"> Your quotation could not be stored due to a system error.</P.';
+                }
+
+            }
+        }
+        ?>
+
+        <form action="add_quotes.php" method="post">
+            <textarea name="quote" rows="5" cols="30">Enter your quotation here.</textarea><br>
+            <input type="submit" name="submit" value="Add This Quote">
+        </form>
+    </body>
+</html>
